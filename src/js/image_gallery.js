c106f3886b76ddd0306db720099aee44e19f4fb9
Paulo Rodrigues <paulor2222@gmail.com>
2018-04-17 18:03:00 -0300

    altered planets structure to simplify

---
+++ src/js/image_gallery.js
@@ -0,0 +1,4 @@
+const current = document.querySelector("#current-img");
+const imgs = document.querySelectorAll('.images img');
+
+imgs.forEach((img) => img.addEventListener('click',(e) => current.src = e.target.src ));
