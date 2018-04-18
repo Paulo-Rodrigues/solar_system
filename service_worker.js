22f609407b5adb9708e23df2c1cc218b438ac479
Paulo Rodrigues <paulor2222@gmail.com>
2018-04-17 22:40:47 -0300

    caching static files to cache api

--- service_worker.js
+++ service_worker.js
@@ -6,7 +6,25 @@ self.addEventListener('install', function(event){
               console.log("PRECACHE");
               cache.addAll([
                   "/",
-                  "/index.html"
+                  "/index.html",
+                  "/src/pages/about.html",
+                  "/src/pages/others.html",
+                  "/src/pages/planets.html",
+                  "/src/pages/rovers.html",
+                  "/src/css/styles.css",
+                  "/src/js/main/bundle.min.js",
+                  "/src/images/icons/favicon.ico",
+                  "/src/images/icons/icons8-conjunto-vazio-50.png",
+                  "/src/images/icons/icons8-conjunto-vazio-144.png",
+                  "/src/images/icons/icons8-conjunto-vazio-192.png",
+                  "/src/images/planets/mercury.jpg",
+                  "/src/images/planets/venus.jpg",
+                  "/src/images/planets/earth.jpg",
+                  "/src/images/planets/mars.jpg",
+                  "/src/images/planets/jupiter.jpg",
+                  "/src/images/planets/saturn.jpg",
+                  "/src/images/planets/uranus.jpg",
+                  "/src/images/planets/neptune.jpg",
               ])
           })
     );
