80e8b56155f009959b889ee6624c4826326e8fbd
Paulo Rodrigues <paulor2222@gmail.com>
2018-04-25 11:18:38 -0300

    add modals

---
+++ src/js/modal.js
@@ -0,0 +1,23 @@
+// var modalBtns = [...document.querySelectorAll(".button")];
+// gulp does not process es6 syntax
+var modalBtns = document.querySelectorAll(".button-modal");
+modalBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.getAttribute('data-modal');
+    document.getElementById(modal).style.display = "block";
+  }
+});
+
+var closeBtns = document.querySelectorAll(".close-modal");
+closeBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.closest('.modal');
+    modal.style.display = "none";
+  }
+});
+
+window.onclick = function(event) {
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
+  }
+}
